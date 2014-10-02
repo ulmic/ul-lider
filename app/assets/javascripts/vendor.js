9e3de2a88c84b12fee0ead807b043125ea3df918
Dmitry Davydov <haudvd@gmail.com>
2014-10-02 20:17:59 +0400

    add require js and add vendor.js

---
+++ app/assets/javascripts/vendor.js
@@ -0,0 +1,5 @@
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap
+//= require bootstrap-switch/dist/js/bootstrap-switch
+//= require jasny-bootstrap/js/fileinput
--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,4 @@
+# -*- coding: undecided -*-
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
@@ -6,18 +7,28 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+load "lib/configus.rb"
+
 module UlLider
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += Dir[
+      "#{config.root}/lib/**/"
+    ]
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Moscow'
 
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.available_locales = [:en, :ru]
+    config.i18n.default_locale = :ru
+
+    config.generators do |g|
+      g.template_engine :haml
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.decorator false
+    end
+
   end
 end
