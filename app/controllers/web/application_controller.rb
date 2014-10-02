365d4973f2c95582b94fb18380e20dbc67cfb944
Dmitry Davydov <haudvd@gmail.com>
2014-10-02 21:42:21 +0400

    add to web::application

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,2 +1,17 @@
 class Web::ApplicationController < ApplicationController
+  include Concerns::FlashHelper
+  include Concerns::TitleHelper
+  include Concerns::RedirectHelper
+
+  helper_method :title
+
+  #before_action :required_basic_auth! if Rails.env.staging?
+
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  before_action do
+    title("Ul-lider")
+  end
 end
