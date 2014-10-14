9dc8c912d88f5e24f3f2556fdb3f9f4a55b5d77b
Dmitry Davydov <haudvd@gmail.com>
2014-10-14 23:33:02 +0400

    add secrets.yml to shared

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,6 +37,7 @@ set :ssh_options, {
 
 # Default value for :linked_files is []
 # set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/secrets.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
