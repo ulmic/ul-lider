5cd982d174402d39558c035583a53fdacebab7a0
pavel <kalashnikovisme@gmail.com>
2014-10-19 13:32:01 +0400

    try fix bug

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141019092306) do
+ActiveRecord::Schema.define(version: 20141019093024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 20141019092306) do
     t.text     "postcode"
     t.text     "creative_work_url"
     t.text     "role"
-    t.text     "creative_work"
+    t.string   "creative_work"
   end
 
 end
