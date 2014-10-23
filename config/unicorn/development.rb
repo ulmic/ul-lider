323e9635a7439d05c60a685915452f2cb257dfbd
Dmitry Davydov <haudvd@gmail.com>
2014-10-23 22:20:04 +0400

    add unicorn development config

---
+++ config/unicorn/development.rb
@@ -0,0 +1,29 @@
+app_path = "./"
+working_directory "#{app_path}"
+
+worker_processes 1
+
+listen "127.0.0.1:3000"
+
+timeout 15
+# preload_app true
+
+stderr_path "#{app_path}/log/unicorn.stderr.log"
+stdout_path "#{app_path}/log/unicorn.stdout.log"
+
+# FIXME
+pid "#{app_path}/tmp/pids/unicorn.pid"
+
+before_exec do |server|
+  ENV['BUNDLE_GEMFILE'] = "#{working_directory}/Gemfile"
+end
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
