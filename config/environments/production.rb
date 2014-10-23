86cc21dc248e9295cfcce01be39bc2ef37486c92
pavel <kalashnikovisme@gmail.com>
2014-10-24 00:57:11 +0400

    return production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,8 +28,6 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.assets.precompile =  ['*.js', '*.css', '*.css.sass', '*.js.coffee']
-  config.serve_static_assets = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
