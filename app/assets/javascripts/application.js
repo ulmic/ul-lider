e7c84c76ec1fcda87efc0b748e0ea0c4da8b0ebe
pavel <kalashnikovisme@gmail.com>
2014-10-24 02:12:23 +0400

    add reveal password button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,12 +15,25 @@
 //= require pickers
 //= require glyphicons
 //= require ckeditor/init
+//= require i18n_setup
 //
-$(document).on('ready page:change', function() {
+$(document).ready(function() {
   $('.datepicker').datetimepicker({
     pickTime: false
   });
   $('.link').click(function() {
     location.href = $(this).attr('data-href');
+  });
+  $('.password_reveal').click(function(e) {
+    e.preventDefault();
+    password_input_class = 'password';
+    $password_input = $('input.' + password_input_class).first();
+    if ($password_input.prop('type') == 'password') {
+      $password_input.prop('type', 'text');
+      $(this).html(I18n.t('javascript.helpers.password_hide'));
+    } else {
+      $password_input.prop('type', 'password');
+      $(this).html(I18n.t('javascript.helpers.password_reveal'));
+    }
   });
 });
