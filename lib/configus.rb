060371a8f1a75b2d2d0b57a02d6b5d46b494740e
pavel <kalashnikovisme@gmail.com>
2014-10-24 14:22:08 +0400

    add more links on footer

--- lib/configus.rb
+++ lib/configus.rb
@@ -1,6 +1,11 @@
 Configus.build Rails.env do
   env :production do
-
+    chrome_extension do
+      url 'https://chrome.google.com/webstore/detail/%D1%8F-%D0%BB%D0%B8%D0%B4%D0%B5%D1%80/lehbmnengpcoaledhaodhjcmibleffmd/'
+    end
+    sites do
+      year_2014 'http://2014.ul-lider.ru'
+    end
     mailer do
       from "info@ul-lider.ru"
     end
