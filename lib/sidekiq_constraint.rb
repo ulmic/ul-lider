6d61722dbd114a62fe7040d22618d0501cc1800f
Dmitry Davydov <haudvd@gmail.com>
2014-10-28 23:06:05 +0300

    add sidekiq web

---
+++ lib/sidekiq_constraint.rb
@@ -0,0 +1,7 @@
+class SidekiqConstraint
+  def matches?(request)
+    return false unless request.session[:user_id]
+    user = User.find request.session[:user_id]
+    user && user.role.admin?
+  end
+end
