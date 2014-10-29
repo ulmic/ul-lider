32f657935263e170301f1bd938511d02df795d6f
pavel <kalashnikovisme@gmail.com>
2014-10-29 18:45:45 +0300

    add admin password

--- app/types/user_edit_by_admin_type.rb
+++ app/types/user_edit_by_admin_type.rb
@@ -2,9 +2,8 @@ class UserEditByAdminType < User
   include ApplicationType
 
   validates :email, presence: true
-#   validates :accept_agreement, acceptance: true
 
-  permit :email, :avatar, :first_name, :last_name, :birth_date, :municipality, :patronymic, :school, :group, :mobile_phone, :locality, :postcode, :creative_work, :url_creative_work, :role
+  permit :email, :password, :avatar, :first_name, :last_name, :birth_date, :municipality, :patronymic, :school, :group, :mobile_phone, :locality, :postcode, :creative_work, :url_creative_work, :role
 
   def email=(email)
     write_attribute(:email, email.mb_chars.downcase)
