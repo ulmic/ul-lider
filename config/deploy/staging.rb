8715c9cc7937b41d1d047af9341e2b4fb29c3abc
Dmitry Davydov <haudvd@gmail.com>
2014-10-30 22:44:06 +0300

    add staging configs

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,6 @@
 server 'new.ul-lider.ru', user: 'lider', roles: %w{web app db}
 
+set :deploy_to, '/srv/lider_v2_stg'
+set :unicorn_pid, "#{shared_path}/tmp/pids/unicorn.pid"
 set :rails_env, :staging
 set :branch, "staging"
