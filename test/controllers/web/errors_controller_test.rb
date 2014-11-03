4242c921bd6e91ca1e94f5eb22cea29f448068fd
pavel <kalashnikovisme@gmail.com>
2014-11-04 02:43:13 +0300

    add more tests

--- test/controllers/web/errors_controller_test.rb
+++ test/controllers/web/errors_controller_test.rb
@@ -4,6 +4,12 @@ class Web::ErrorsControllerTest < ActionController::TestCase
   test "should not found" do
     get :not_found
     assert_response 404
+    get :not_found, anchor: 'view_1'
+    assert_response 404
+    get :not_found, anchor: 'view_2'
+    assert_response 404
+    get :not_found, anchor: 'view_3'
+    assert_response 404
   end
   test "should forbidden" do
     get :forbidden
