85560d7fe56635aada9a717f703c3ec548e5abb4
Dmitry Davydov <haudvd@gmail.com>
2014-11-17 18:03:34 +0300

    minimal defence of sidekiq deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,3 +5,13 @@ set :deploy_to, '/srv/lider_v2_stg'
 set :unicorn_pid, "#{shared_path}/tmp/pids/unicorn.pid"
 set :rails_env, :staging
 set :branch, "develop"
+
+namespace :deploy do
+  task :restart do
+    puts "DONT DEPLOY TO STAGING!!!! DONT!!!"
+    invoke 'unicorn:stop'
+    invoke 'unicorn:start'
+  end
+
+  after :publishing, :restart
+end
