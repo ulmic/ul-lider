7eecbd38d9a9dc8bcd70f09a5c1ab236103e4223
pavel <kalashnikovisme@gmail.com>
2014-12-22 17:18:54 +0300

    redirecting to users in admin panel

--- app/controllers/web/admin/welcome_controller.rb
+++ app/controllers/web/admin/welcome_controller.rb
@@ -1,7 +1,5 @@
 class Web::Admin::WelcomeController < Web::Admin::ApplicationController
   def index
-    @confirmed_by_admin_users_count = User.participants.confirmed.count
-    @new_users_count = User.participants.waiting_confirmation.count
-    @waiting_approve_count = User.participants.active.count
+    redirect_to admin_users_path
   end
 end
