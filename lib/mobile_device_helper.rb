23a10f0f972f0304f423024adae9662d7928f210
pavel <kalashnikovisme@gmail.com>
2015-01-04 06:02:34 +0300

    Add mobile device images

---
+++ lib/mobile_device_helper.rb
@@ -0,0 +1,9 @@
+module MobileDeviceHelper
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == '1'
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+end
