2157c72f7ddf43ce2c64e879fa615fb88556cd2c
pavel <kalashnikovisme@gmail.com>
2015-01-14 08:54:24 +0300

    add count of new users in navbar

--- app/helpers/web/welcome_helper.rb
+++ app/helpers/web/welcome_helper.rb
@@ -2,4 +2,8 @@ module Web::WelcomeHelper
   def winner_info(index)
     YAML.load_file("#{Rails.root}/lib/yaml/winners_info.yml")["info"][index]
   end
+  def new_users_count
+    count = User.participants.where(state: :active).count
+    count unless count == 0
+  end
 end
