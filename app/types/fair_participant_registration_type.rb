fb2d58cbac4ad8904aed522129d06d6e0564681a
pavel <kalashnikovisme@gmail.com>
2015-01-19 17:14:58 +0300

    add shkr

--- app/types/fair_participant_registration_type.rb
+++ app/types/fair_participant_registration_type.rb
@@ -1,7 +1,7 @@
 class FairParticipantRegistrationType < User
   include ApplicationType
 
-  attr_accessor :i_am_not_from_73reg
+  attr_accessor :i_am_not_from_73reg, :i_am_reserve_school
 
   validates :email, presence: true
   validates :birth_date, presence: true
@@ -15,9 +15,17 @@ class FairParticipantRegistrationType < User
   validates :fair_goals, presence: true
   validates :region, presence: true
 
-  permit :email, :avatar, :first_name, :last_name, :birth_date, :municipality, :patronymic, :school, :group, :mobile_phone, :locality, :postcode, :state, :accept_agreement, :life_goals, :fair_goals, :region, :i_am_not_from_73reg, :avatar_cache, :role
+  permit :email, :avatar, :first_name, :last_name, :birth_date, :municipality, :patronymic, :school, :group, :mobile_phone, :locality, :postcode, :state, :accept_agreement, :life_goals, :fair_goals, :region, :i_am_not_from_73reg, :avatar_cache, :role, :i_am_reserve_school
 
   def email=(email)
     write_attribute(:email, email.mb_chars.downcase)
+  end
+
+  def i_am_reserve_school=(i_am_reserve_school)
+    if i_am_reserve_school
+      write_attribute :role, :reserve_school
+    else
+      write_attribute :role, :fair_participant
+    end
   end
 end
