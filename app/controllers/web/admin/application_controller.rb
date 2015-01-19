0fe1e21b8abbcb1c84dfcdd33313bfc4c85ec6b6
pavel <kalashnikovisme@gmail.com>
2015-01-19 07:29:06 +0300

    some adds

--- app/controllers/web/admin/application_controller.rb
+++ app/controllers/web/admin/application_controller.rb
@@ -1,4 +1,11 @@
 class Web::Admin::ApplicationController < Web::ApplicationController
   before_action :authenticate_admin!
+  before_action :check_requests
+
+  def check_requests
+    @contest_parts = User.participants.where state: :active
+    @fair_parts = User.fair_participants.where state: :active
+  end
+
   layout 'admin'
 end
