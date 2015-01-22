1eef408b36026dc6a2d84666aec4de53be966766
pavel <kalashnikovisme@gmail.com>
2015-01-22 07:56:42 +0300

    reverse all users

--- app/controllers/web/admin/fair_participants_controller.rb
+++ app/controllers/web/admin/fair_participants_controller.rb
@@ -1,10 +1,10 @@
 class Web::Admin::FairParticipantsController < Web::Admin::ApplicationController
   def index
-    @confirmed_by_admin_requests = UserDecorator.decorate_collection User.fair_participants.where state: :confirmed_by_admin
-    @waiting_confirmation_requests = UserDecorator.decorate_collection User.fair_participants.where state: :waiting_confirmation
-    @active_requests = UserDecorator.decorate_collection User.fair_participants.where state: :active
-    @inactive_requests = UserDecorator.decorate_collection User.fair_participants.where state: :inactive
-    @reserve_schoolers_requests = UserDecorator.decorate_collection User.reserve_schoolers
+    @confirmed_by_admin_requests = UserDecorator.decorate_collection User.fair_participants.where(state: :confirmed_by_admin).reverse
+    @waiting_confirmation_requests = UserDecorator.decorate_collection User.fair_participants.where(state: :waiting_confirmation).reverse
+    @active_requests = UserDecorator.decorate_collection User.fair_participants.where(state: :active).reverse
+    @inactive_requests = UserDecorator.decorate_collection User.fair_participants.where(state: :inactive).reverse
+    @reserve_schoolers_requests = UserDecorator.decorate_collection User.reserve_schoolers.reverse
   end
 
   def new
