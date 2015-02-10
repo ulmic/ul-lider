7661b500903a3b248cf0ab98c7690a0a8b1538b1
pavel <kalashnikovisme@gmail.com>
2015-02-10 23:00:23 +0300

    fix count of parts

--- app/controllers/web/welcome_controller.rb
+++ app/controllers/web/welcome_controller.rb
@@ -1,11 +1,11 @@
 class Web::WelcomeController < Web::ApplicationController
   def index
     @last_news = NewsDecorator.decorate_collection News.published.last 3
-    @participants = UserDecorator.decorate_collection User.participants.shuffle.first 13
+    @participants = UserDecorator.decorate_collection User.participants.shuffle
   end
 
   def index2
     @last_news = NewsDecorator.decorate_collection News.published.last 3
-    @participants = UserDecorator.decorate_collection User.participants.shuffle.first 13
+    @participants = UserDecorator.decorate_collection User.participants.shuffle
   end
 end
