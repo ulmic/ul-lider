b23aef5733a617bd6df93b8ae8036c150a40f564
AlisVero <soxat73rus@gmail.com>
2015-02-23 03:26:51 +0300

    add english locale

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module UlLider
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Moscow'
-    config.i18n.available_locales = [:ru]
+    config.i18n.available_locales = [:ru, :en]
     config.i18n.default_locale = :ru
     config.assets.enabled = true
     config.assets.initialize_on_precompile = true
