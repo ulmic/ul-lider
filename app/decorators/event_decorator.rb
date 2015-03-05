bda10190cdd40f7838be0ef1c18fdb02a2eea98e
pavel <kalashnikovisme@gmail.com>
2015-03-05 03:20:22 +0300

    add view of event

---
+++ app/decorators/event_decorator.rb
@@ -0,0 +1,11 @@
+class EventDecorator < ApplicationDecorator
+  delegate_all
+
+  def duration
+    if begin_date.day == end_date.day && begin_date.month == end_date.month && begin_date.year == end_date.year
+      "#{I18n.l(begin_date, format: '%d %b %Y')} #{begin_date.to_s.match(/[0-9]{2}:[0-9]{2}/)} - #{end_date.to_s.match(/[0-9]{2}:[0-9]{2}/)}"
+    else
+      "#{I18n.l(begin_date, format: '%d %b %Y %H:%m')} - #{I18n.l(end_date, format: '%d %b %Y %H:%m')}"
+    end
+  end
+end
