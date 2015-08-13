1972ff90c6dbe0362d4828436bd91ef29b7b1c95
pavel <kalashnikovisme@gmail.com>
2015-08-13 19:51:51 +0300

    Some merge

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,23 @@
+//= require moment
+
+$(document).ready(function() {
+  $('.datepicker').datetimepicker({
+    pickTime: false
+  });
+  $('.link').click(function() {
+    location.href = $(this).attr('data-href');
+  });
+  $('.target_blank').attr('target', '_blank');
+  $('.password_reveal').click(function(e) {
+    e.preventDefault();
+    password_input_class = 'password';
+    $password_input = $('input.' + password_input_class).first();
+    if ($password_input.prop('type') == 'password') {
+      $password_input.prop('type', 'text');
+      $(this).html(I18n.t('javascript.helpers.password_hide'));
+    } else {
+      $password_input.prop('type', 'password');
+      $(this).html(I18n.t('javascript.helpers.password_reveal'));
+    }
+  });
+});
