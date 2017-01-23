bbae277ca2ab14cdf2de640642205e60ca2a84c2
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-01-23 04:33:52 +0400

    Add script

---
+++ bin/send_all_current_participants_to_ulmic.rb
@@ -0,0 +1,11 @@
+User.where('created_at > ?', DateTime.new(2016, 12, 19)).each do |user|
+  user_attributes = user.attributes
+  [ 'created_at', 'updated_at', 'birth_date' ].each do |time_obj|
+    user_attributes[time_obj] = user_attributes[time_obj].to_i
+  end
+  user_attributes['fields'] = {}
+  user.model.fields.each_with_index do |field, index|
+    user_attributes['fields'][index.to_s] = field.attributes.except('created_at', 'updated_at')
+  end
+  UlmicUserJob.perform_now user_attributes
+end
